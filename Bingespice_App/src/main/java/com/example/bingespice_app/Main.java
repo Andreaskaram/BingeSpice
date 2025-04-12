5befd99e65a48a338ca151b5148e52451adceca4
Alexandros <achalampakis@gmail.com>
2025-04-12 15:13:14 +0300

    .

--- Bingespice_App/src/main/java/com/example/bingespice_app/Main.java
+++ Bingespice_App/src/main/java/com/example/bingespice_app/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Profile.fxml"));
         Scene scene = new Scene(root);
 
         // Set a default size before maximizing
