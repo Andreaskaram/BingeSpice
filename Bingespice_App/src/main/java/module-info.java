d3e28df4c2de418df19e47bf77cdde7809d7d1e9
Andreaskaram <andrekaramalikis@gmail.com>
2025-06-21 20:07:22 +0300

    Update module-info.java

--- Bingespice_App/src/main/java/module-info.java
+++ Bingespice_App/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.bingespice_app {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    //requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
